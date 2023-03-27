dd9aab604026cb6319621a64a9a5645b441300a8
votrepoesie <vrli@uchicago.edu>
2023-03-27 17:37:12 -0500

    more fun

--- cam.js
+++ cam.js
@@ -36,6 +36,18 @@ async function draw () {
       gif3.style.left = nose.x - gif3.width / 2 + 10 + 'px'
       gif3.style.top = nose.y - gif3.height / 2 + 'px'
 
+      const face = document.querySelector('.face')
+      face.style.left = nose.x - 60 + 'px'
+      face.style.top = nose.y + - 100 + 'px'
+
+      const leftCheek = poses[0].keypoints[8]
+      gif4.style.left = leftCheek.x - 20 + 'px'
+      gif4.style.top = leftCheek.y + 'px'
+
+      const rightCheek = poses[0].keypoints[7]
+      gif5.style.left = rightCheek.x - 20 + 'px'
+      gif5.style.top = rightCheek.y + 'px'
+
       const mouth = poses[0].keypoints[9] 
       gif1.style.left = mouth.x - gif1.width / 2 - 15 + 'px'
       gif1.style.top = mouth.y - gif1.height / 2 + 20 + 'px'
@@ -48,7 +60,7 @@ async function setup () {
     gif.src = 'eye.png'
     gif.style.position = 'absolute'
     document.body.appendChild(gif)
-    gif.style.height = '70px'
+    gif.style.height = '60px'
 
     gif1 = document.createElement('img')
     gif1.src = 'mouth.png'
@@ -60,13 +72,39 @@ async function setup () {
     gif2.src = 'eye.png'
     gif2.style.position = 'absolute'
     document.body.appendChild(gif2)
-    gif2.style.height = '70px'
+    gif2.style.height = '60px'
 
     gif3 = document.createElement('img')
     gif3.src = 'nose.png'
     gif3.style.position = 'absolute'
+    gif3.id = 'nose'
     document.body.appendChild(gif3)
     gif3.style.height = '50px'
+
+
+    gif4 = document.createElement('img')
+    gif4.src = 'flowers.gif'
+    gif4.style.position = 'absolute'
+    document.body.appendChild(gif4)
+    gif4.style.height = '70px'
+
+    gif5 = document.createElement('img')
+    gif5.src = 'flowers.gif'
+    gif5.style.position = 'absolute'
+    document.body.appendChild(gif5)
+    gif5.style.height = '70px'
+
+    gif6 = document.createElement('img')
+    gif6.src = 'star.gif'
+    gif6.style.position = 'absolute'
+    document.body.appendChild(gif6)
+    gif6.style.height = '70px' 
+
+    gif7 = document.createElement('img')
+    gif7.src = 'star.gif'
+    gif7.style.position = 'absolute'
+    document.body.appendChild(gif7)
+    gif7.style.height = '70px'
 
     video = await addVideo()
   	detector = await setupModel()
